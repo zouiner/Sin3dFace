6b21d18f1dba92b2a61ac55490e2100c12314b0f
P Saengduean <patipol.saeng@gmail.com>
2025-02-10 22:32:09 +0000

    Start repository

---
+++ main_distill.py
@@ -0,0 +1,54 @@
+import argparse
+from omegaconf import OmegaConf
+from trainer import TrainerDistillDifIR as TrainerDistill
+
+
+def get_parser(**parser_kwargs):
+    parser = argparse.ArgumentParser(**parser_kwargs)
+    parser.add_argument(
+            "--save_dir",
+            type=str,
+            default="./saved_logs",
+            help="Folder to save the checkpoints and training log",
+            )
+    parser.add_argument(
+            "--resume",
+            type=str,
+            const=True,
+            default="",
+            nargs="?",
+            help="Resume from the save_dir or checkpoint",
+            )
+    parser.add_argument(
+            "--cfg_path",
+            type=str,
+            default="./configs/realsr_swinunet_realesrgan256.yaml",
+            help="Configs of yaml file",
+            )
+    parser.add_argument(
+            "--steps",
+            type=int,
+            default=15,
+            help="Hyper-parameters of diffusion steps",
+            )
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == "__main__":
+    args = get_parser()
+
+    configs = OmegaConf.load(args.cfg_path)
+    configs.diffusion.params.steps = args.steps
+
+    # merge args to config
+    for key in vars(args):
+        if key in ['cfg_path', 'save_dir', 'resume', ]:
+            configs[key] = getattr(args, key)
+
+    trainer = TrainerDistill(configs)
+    trainer.train()
+
+# export OMP_NUM_THREADS=1
+# python3 -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m torch.distributed.run --nproc_per_node=3 main_distill.py --cfg_path configs/SinSR_vggface2.yaml --save_dir logs/SinSR
+# python3 -m torch.distributed.run --nproc_per_node=3 main_distill.py --cfg_path configs/SinSR_vggface2.yaml --save_dir logs/SinSR
---
+++ run_gpuplus.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=SinSR_test_gpuplus             # Job name
+#SBATCH --mail-type=BEGIN,END,FAIL             # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=moonlight.dum1@gmail.com          # Where to send mail
+#SBATCH --ntasks=1                             # Run a single task...
+#SBATCH --cpus-per-task=1                      # ...with a single CPU
+#SBATCH --mem=16gb                             # Job memory request
+#SBATCH --time=3-00:00:00                      # Time limit (DD-HH:MM:SS)
+#SBATCH --output=cuda_log/SinSR_test_gpuplus_%j.log      # Standard output and error log
+#SBATCH --partition=gpuplus                        # Select the GPU nodes... (, interactive, gpu , gpuplus)  
+#SBATCH --gres=gpu:2                          # ...and the Number of GPUs
+#SBATCH --account=its-gpu-2023                 # Run job under project <project>
+
+module purge
+module load GCC/12.2.0
+module load Miniconda3 # for conda, if using venv you wont need this
+
+# This is just printing stuff you don't really need these lines
+echo `date`: executing gpu_test on host $HOSTNAME with $SLURM_CPUS_ON_NODE cpu cores echo 
+cudaDevs=$(echo $CUDA_VISIBLE_DEVICES | sed -e 's/,/ /g')
+echo I can see GPU devices $CUDA_VISIBLE_DEVICES
+echo
+
+source ~/.bashrc
+
+conda activate SinSR # Your conda environment here 
+# or source your .venv environment here if using venv
+
+# now run the python script
+export OMP_NUM_THREADS=1
+torchrun --nproc_per_node=3 main_distill.py --cfg_path configs/SinSR_vggface2.yaml --save_dir logs/SinSR
+
+# to run the command: sbatch run_gpuplus.sh
